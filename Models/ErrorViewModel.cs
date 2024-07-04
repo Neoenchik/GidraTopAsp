281b28e7b2ac10f27a43f6281f80fcddd323a654
Neoenchik <102302057+Neoenchik@users.noreply.github.com>
2024-07-04 23:57:50 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace GidraTop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId
+    {
+        get; set;
+    }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
